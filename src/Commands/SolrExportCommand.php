b3879f88ce9684fde9a59b87220e680c7f364e9c
Minh Duc Nguyen <dekarvn@gmail.com>
2016-10-06 14:21:06 +1100

    Added progress bar

--- src/Commands/SolrExportCommand.php
+++ src/Commands/SolrExportCommand.php
@@ -4,11 +4,13 @@ namespace MinhD\SolrClient\Commands;
 
 use MinhD\SolrClient\SolrClient;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Stopwatch\Stopwatch;
 
 class SolrExportCommand extends Command
 {
@@ -69,36 +71,45 @@ class SolrExportCommand extends Command
 
         // find out how big this is
         $payload = $solr->search([
-           'q' => '*', 'rows' => '0'
+            'q' => '*',
+            'rows' => '0'
         ]);
         $numFound = $payload->getNumFound();
 
-        $output->writeln("There are ".$numFound. " records to export.");
+        $output->writeln("There are " . $numFound . " records to export.");
 
-        ini_set('memory_limit','256M');
+        ini_set('memory_limit', '256M');
         $fs = new Filesystem();
+        $stopwatch = new Stopwatch();
 
         $continue = true;
         $start = "*";
         $i = 1;
-        $progress = 0;
 
+        $progressBar = new ProgressBar($output, $numFound);
+        $stopwatch->start('download');
         while ($continue) {
             $payload = $solr->cursor($start, $rows);
             $documents = $payload->getDocs('json');
-            $fs->dumpFile($targetDir.'/'.$i.'.json', $documents);
+            $fs->dumpFile($targetDir . '/' . $i . '.json', $documents);
 
-            $progress += sizeof($payload->getDocs());
-            $percentage = ($progress * 100 ) / $numFound;
             if (sizeof($payload->getDocs()) == 0) {
                 $continue = false;
             }
+
             $i++;
             $start = $payload->getNextCursorMark();
-
-            $output->writeln(sizeof($payload->getDocs()). " Downloaded. Next: ". $start. ". Progress: ".$progress. '  ('.round($percentage,2).'%). Memory: '.memory_get_usage());
+            $progressBar->advance(sizeof($payload->getDocs()));
         }
-        $output->writeln('Finished');
+        $progressBar->finish();
+        $event = $stopwatch->stop('download');
 
+        $output->writeln('');
+        $output->writeln(
+            'Finished. Took (' . round($event->getDuration() / 1000, 2) . ')s'
+        );
+        $output->writeln(
+            "Max Memory Usage: " . round($event->getMemory() / 1000, 2) . ' KB'
+        );
     }
 }
